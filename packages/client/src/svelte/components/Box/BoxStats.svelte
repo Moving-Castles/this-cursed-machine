b22c53956e6259e21c9d4544d43f0d44b386a685
pwrstudio <pwrpntr@gmail.com>
2023-10-13 14:48:32 +0200

    design: changes to stats box

--- packages/client/src/svelte/components/Box/BoxStats.svelte
+++ packages/client/src/svelte/components/Box/BoxStats.svelte
@@ -1,29 +1,25 @@
 <script lang="ts">
   // import { playerCore, playerBox } from "../../modules/state"
   import { onWheel } from "../../modules/ui/events"
-  import { EntityType, MaterialType } from "../../modules/state/enums"
+  import {  MaterialType } from "../../modules/state/enums"
   import {
     simulatedPlayerCore,
     simulatedPlayerEnergy,
     boxOutput,
   } from "../../modules/simulator"
-  import { blocksSinceLastResolution } from "../../modules/simulator/"
-  // import { coreIsConnectedToInlet } from "../../modules/simulator/"
-  // import { blockNumber } from "../../modules/network/"
-
-  // If we are not connected to inlet energy is:
-  // Energy = on-chain energy - blocksSinceLastResolution
-  // If we are connected to inlet energy is:
-  // Energy = on-chain energy + blocksSinceLastResolution
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="box-stats" use:onWheel>
+  <p>Worker#24</p>
+  <p class="muted">-------------------</p>
   <p>
-    <span class="muted">You:</span><br /> worker#24 ({#key $simulatedPlayerCore.energy}
-      <span class="green">ENERGY: {$simulatedPlayerEnergy}</span>{/key})
+    {#key $simulatedPlayerCore.energy}
+      <span class="green">ENERGY:{$simulatedPlayerEnergy}</span>
+    {/key}
   </p>
-  <p class="muted">Pool:</p>
+  <p class="muted">-------------------</p>
+  <p class="muted">Produced materials:</p>
   <p>
     {#if Object.entries($boxOutput).length > 0}
       {#each Object.entries($boxOutput) as [type, amount] (type)}
@@ -34,8 +30,6 @@
           </span>
         </span><br />
       {/each}
-    {:else}
-      Empty
     {/if}
   </p>
 </div>
