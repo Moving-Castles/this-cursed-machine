773915f4fed5c8ff66004cda1bbef3364d9db3ce
pwrstudio <pwrpntr@gmail.com>
2023-11-06 14:06:36 +0100

    refactor: move initial transfer to restart

--- packages/contracts/src/systems/TransferSystem.sol
+++ packages/contracts/src/systems/TransferSystem.sol
@@ -15,6 +15,11 @@ contract TransferSystem is System {
   function transfer() public returns (bytes32) {
     bytes32 coreEntity = LibUtils.addressToEntityKey(_msgSender());
 
+    uint32 newLevel = Level.get(coreEntity) + 1;
+
+    // New level needs to be in range 2 to 8
+    require(newLevel > 1 && newLevel < 9, "illegal level");
+
     // Resolve network if we are in pod
     if (CarriedBy.get(coreEntity) != bytes32(0)) {
       LibNetwork.resolve(CarriedBy.get(coreEntity));
@@ -35,40 +40,9 @@ contract TransferSystem is System {
     }
 
     // Level up core entity
-    uint32 newLevel = Level.get(coreEntity) + 1;
     Level.set(coreEntity, newLevel);
 
-    if (newLevel == 1) {
-      // Core is at level 1, enter the pod
-
-      // Set initial energy
-      bytes32 levelEntity = LibLevel.getLevel(newLevel);
-      Energy.set(coreEntity, Energy.get(levelEntity));
-
-      // Create box entity
-      bytes32 boxEntity = LibBox.create(newLevel);
-
-      // Create Inlet
-      bytes32 inletEntity = LibEntity.create(MACHINE_TYPE.INLET);
-      CarriedBy.set(inletEntity, boxEntity);
-      LibPort.create(inletEntity, PORT_TYPE.OUTPUT);
-
-      // Place core in box
-      CarriedBy.set(coreEntity, boxEntity);
-
-      // Create ports on core
-      LibPort.create(coreEntity, PORT_TYPE.INPUT);
-      LibPort.create(coreEntity, PORT_TYPE.OUTPUT);
-      LibPort.create(coreEntity, PORT_TYPE.OUTPUT);
-
-      // Create Outlet
-      bytes32 outletEntity = LibEntity.create(MACHINE_TYPE.OUTLET);
-      CarriedBy.set(outletEntity, boxEntity);
-      LibPort.create(outletEntity, PORT_TYPE.INPUT);
-
-      // Return box
-      return boxEntity;
-    } else if (newLevel == 8) {
+    if (newLevel == 8) {
       // Core is at level 8, progression done
       // Remove core from box
       CarriedBy.deleteRecord(coreEntity);
@@ -77,7 +51,7 @@ contract TransferSystem is System {
       // Return null-pod
       return bytes32(0);
     } else {
-      // Core is at level 2-8
+      // Core is at level 2-7
       // Level up box
       Level.set(CarriedBy.get(coreEntity), newLevel);
       // Disconnect outlet
