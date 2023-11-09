bb16d0146c26f281bae53e5ff2a7014995c0776f
pwrstudio <pwrpntr@gmail.com>
2023-11-09 21:36:46 +0100

    fix: remove incoming on destroy. close #74

--- packages/contracts/src/systems/DestroySystem.sol
+++ packages/contracts/src/systems/DestroySystem.sol
@@ -14,26 +14,51 @@ contract DestroySystem is System {
     bytes32 coreEntity = LibUtils.addressToEntityKey(_msgSender());
     require(EntityType.get(_machineEntity) == ENTITY_TYPE.MACHINE, "not machine");
 
-    LibNetwork.resolve(coreEntity);
-
     // Get outgoing connections
     bytes32[] memory outgoingConnections = OutgoingConnections.get(_machineEntity);
+    // Get incoming connections
+    bytes32[] memory incomingConnections = IncomingConnections.get(_machineEntity);
 
-    // Iterate through each outgoing connection
-    for (uint256 i = 0; i < outgoingConnections.length; i++) {
-      bytes32 targetMachine = outgoingConnections[i];
-      if (targetMachine != bytes32(0)) {
-        // Get incoming connections of the target machine
-        bytes32[] memory targetIncomingConnections = IncomingConnections.get(targetMachine);
+    if (outgoingConnections.length > 0 || incomingConnections.length > 0) {
+      // Only resolve if the machine is connected
+      LibNetwork.resolve(coreEntity);
 
-        // Iterate through the target machine's incoming connections
-        for (uint256 j = 0; j < targetIncomingConnections.length; j++) {
-          if (targetIncomingConnections[j] == _machineEntity) {
-            // Remove the reference to the destroyed machine
-            targetIncomingConnections[j] = bytes32(0);
-            // Update the target machine's incoming connections
-            IncomingConnections.set(targetMachine, targetIncomingConnections);
-            break;
+      // Iterate through each incoming connection
+      for (uint256 i = 0; i < incomingConnections.length; i++) {
+        bytes32 sourceMachine = incomingConnections[i];
+        if (sourceMachine != bytes32(0)) {
+          // Get outgoing connections of the source machine
+          bytes32[] memory sourceOutgoingConnections = OutgoingConnections.get(sourceMachine);
+
+          // Iterate through the target machine's outgoing connections
+          for (uint256 j = 0; j < sourceOutgoingConnections.length; j++) {
+            if (sourceOutgoingConnections[j] == _machineEntity) {
+              // Remove the reference to the destroyed machine
+              sourceOutgoingConnections[j] = bytes32(0);
+              // Update the source machine's outgoing connections
+              OutgoingConnections.set(sourceMachine, sourceOutgoingConnections);
+              break;
+            }
+          }
+        }
+      }
+
+      // Iterate through each outgoing connection
+      for (uint256 i = 0; i < outgoingConnections.length; i++) {
+        bytes32 targetMachine = outgoingConnections[i];
+        if (targetMachine != bytes32(0)) {
+          // Get incoming connections of the target machine
+          bytes32[] memory targetIncomingConnections = IncomingConnections.get(targetMachine);
+
+          // Iterate through the target machine's incoming connections
+          for (uint256 j = 0; j < targetIncomingConnections.length; j++) {
+            if (targetIncomingConnections[j] == _machineEntity) {
+              // Remove the reference to the destroyed machine
+              targetIncomingConnections[j] = bytes32(0);
+              // Update the target machine's incoming connections
+              IncomingConnections.set(targetMachine, targetIncomingConnections);
+              break;
+            }
           }
         }
       }
