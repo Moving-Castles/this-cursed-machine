fa6099a88f82751e1042c9780cb8f13746362e51
PWR <pwrpntr@gmail.com>
2024-04-03 11:51:57 +0200

    quick fix: hide depot attachements when leaving pod

--- packages/client/src/svelte/components/Main/Tabs/Pod/Attachments/Attachments.svelte
+++ packages/client/src/svelte/components/Main/Tabs/Pod/Attachments/Attachments.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
   import { depotAttachments } from "@modules/state/simulated/stores"
   import Attachment from "./Attachment.svelte"
+  import { activeTab } from "@svelte/modules/ui/stores"
 
   let [innerWidth, innerHeight] = [0, 0]
+
+  // Quick fix: only show when the first tab (pod) is active
+  $: visible = $activeTab === 0
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
 <svg
   class="depot-connections"
+  class:visible
   width={innerWidth}
   height={innerHeight}
   viewBox="0 0 {innerWidth} {innerHeight}"
@@ -18,7 +23,7 @@
   {/each}
 </svg>
 
-<style>
+<style lang="scss">
   .depot-connections {
     position: absolute;
     pointer-events: none;
@@ -27,5 +32,10 @@
     width: 100vw;
     height: 100vh;
     z-index: 9;
+
+    display: none;
+    &.visible {
+      display: block;
+    }
   }
 </style>
