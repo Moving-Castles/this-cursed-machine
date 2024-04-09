77175518282cd5b8a9e47acd58c3b4957a82becb
PWR <pwrpntr@gmail.com>
2024-04-09 09:04:43 +0200

    remove unused sanity data

--- packages/client/src/svelte/modules/content/index.ts
+++ packages/client/src/svelte/modules/content/index.ts
@@ -9,16 +9,8 @@ export type StaticContent = {
       content: any[]
     }
   }
-  spawning: {
-    content: {
-      content: any[]
-    }
-  }
   machines: any[]
-  levels: any[]
   materials: any[]
-  commands: any[]
-  map: any
   tutorial: any
   messages: any[]
 }
@@ -31,22 +23,14 @@ export const staticContent = writable({} as StaticContent)
 
 export async function initStaticContent() {
   const loading = await loadData("*[_type == 'loading'][0]", {})
-  const spawning = await loadData("*[_type == 'spawning'][0]", {})
-  const levels = await loadData("*[_type == 'level']", {})
   const machines = await loadData("*[_type == 'machine']", {})
   const materials = await loadData("*[_type == 'material']", {})
-  const commands = await loadData("*[_type == 'command']", {})
-  const map = await loadData("*[_type == 'map'][0]", {})
   const tutorial = await loadData("*[_type == 'tutorial'][0]", {})
   const messages = await loadData("*[_type == 'message']", {})
   staticContent.set({
     loading,
-    spawning,
-    levels,
     machines,
     materials,
-    commands,
-    map,
     tutorial,
     messages,
   })
