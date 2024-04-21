4c32bdd4d1f218c149877e8d53092091f53bd0a4
PWR <pwrpntr@gmail.com>
2024-04-21 15:06:26 +0200

    new indexer and offchain server

--- packages/client/src/svelte/modules/signal/constants.ts
+++ packages/client/src/svelte/modules/signal/constants.ts
@@ -1,3 +1,3 @@
 // export const SIGNAL_SERVER_URL = "ws://localhost:9001"
-export const SIGNAL_SERVER_URL = "wss://mc.rttskr.com"
+export const SIGNAL_SERVER_URL = "wss://offchain.mc-infra.com"
 export const MESSAGE = "mc"
