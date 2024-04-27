216ea9804b524a89eef5a889e8382fbbae56edf4
PWR <pwrpntr@gmail.com>
2024-04-27 16:25:38 +0200

    add new test orders

--- packages/contracts/src/libraries/init/LibInit.sol
+++ packages/contracts/src/libraries/init/LibInit.sol
@@ -55,5 +55,11 @@ library LibInit {
 
     // 50 BLOOD => 200 points
     LibOrder.create(_adminAddress, PublicMaterials.BLOOD, 50 * ONE_UNIT, false, 0, 200 * ONE_UNIT, 0, 20);
+
+    // 50 BLOOD_MEAL => 300 points
+    LibOrder.create(_adminAddress, PublicMaterials.BLOOD_MEAL, 50 * ONE_UNIT, false, 0, 300 * ONE_UNIT, 0, 20);
+
+    // 50 UREA => 500 points
+    LibOrder.create(_adminAddress, PublicMaterials.UREA, 50 * ONE_UNIT, false, 0, 500 * ONE_UNIT, 0, 10);
   }
 }
