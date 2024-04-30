8f0c78d6e6b6880d29cf507df9d1aa84a59dc4d5
PWR <pwrpntr@gmail.com>
2024-04-30 17:32:48 +0200

    quick fix: offchain communication init

--- packages/client/src/svelte/modules/ui/assistant/index.ts
+++ packages/client/src/svelte/modules/ui/assistant/index.ts
@@ -71,7 +71,7 @@ function updateConditions() {
   // Sequential steps will have no skip value, and steps that can be done in another order will have the max skip step index defined. Skips are only looking forward
   const ADVANCE_CONDITIONS = [
     // IN SEQUENCE
-    { index: 0, type: "wait", value: 4000 },
+    { index: 0, type: "wait", value: 3000 },
     { index: 1, type: "command", value: ["blink", "."] },
     { index: 2, type: "command", value: ["blink", "."] },
     { index: 3, type: "tab", value: [1] },
